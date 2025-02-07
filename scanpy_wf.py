762f611fbc147a3684e7904ca5be763a9a9f86c0
Pipp8 <cattaneo@unisa.it>
2025-02-07 18:25:19 +0000

    update

--- scanpy_wf.py
+++ scanpy_wf.py
@@ -4,11 +4,12 @@ import string
 import numpy as np
 import sys
 from pathlib import Path
+from  datetime import  datetime
 
 # import library
 import pandas as pd
 import scanpy as sc
-import time
+
 import anndata
 import openpyxl
 from sklearn.metrics import adjusted_rand_score
@@ -35,22 +36,22 @@ def runTest(input: string):
 
     # data ####
 
-    start_time = time.time()
+    start_time = datetime.now()
     adata = sc.read_h5ad(input)
     adata.var_names_make_unique()  # this is unnecessary if using `var_names='gene_ids'` in `sc.read_10x_mtx`
     print( "Input size: ", adata.shape, flush=True)
     adata
 
-    end_time = time.time()
+    end_time = datetime.now()
     time_elapsed = end_time - start_time
-    print("Loading data Time Elapsed:", time_elapsed, flush=True)
+    print(f"{end_time}: Loading data Time Elapsed:", time_elapsed, flush=True)
     time_sc.iloc[0, 0] = time_elapsed
 
     # Show those genes that yield the highest fraction of counts in each single cell, across all cells.
     sc.pl.highest_expr_genes(adata, n_top=20, )
 
     # find mitocondrial genes ####
-    start_time = time.time()
+    start_time = datetime.now()
 
     adata.var['mt'] = adata.var_names.str.startswith('MT-')  # annotate the group of mitochondrial genes as 'mt'
     sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
@@ -58,13 +59,13 @@ def runTest(input: string):
     sc.pl.violin(adata, ['n_genes_by_counts', 'total_counts', 'pct_counts_mt'],
                  jitter=0.4, multi_panel=True)
 
-    end_time = time.time()
+    end_time = datetime.now()
     time_elapsed = end_time - start_time
-    print("Time Elapsed:", time_elapsed, flush=True)
+    print(f"{end_time}: Time Elapsed:", time_elapsed, flush=True)
     time_sc.iloc[1, 0] = time_elapsed
 
     # filter data sulle cellule, profondità di sequenziamento####
-    start_time = time.time()
+    start_time = datetime.now()
 
     sc.pp.filter_cells(adata, min_genes=200)
     sc.pp.filter_genes(adata, min_cells=3)
@@ -75,25 +76,25 @@ def runTest(input: string):
     adata = adata[adata.obs.n_genes_by_counts < 5000, :]
     adata = adata[adata.obs.pct_counts_mt < 5, :]
 
-    end_time = time.time()
+    end_time = datetime.now()
     time_elapsed = end_time - start_time
     adata
-    print("Filtering Time Elapsed:", time_elapsed, flush=True)
+    print(f"{end_time}: Filtering Time Elapsed:", time_elapsed, flush=True)
     time_sc.iloc[2, 0] = time_elapsed
 
     # normalization ####
-    start_time = time.time()
+    start_time = datetime.now()
 
     sc.pp.normalize_total(adata, target_sum=1e4)
     sc.pp.log1p(adata)
 
-    end_time = time.time()
+    end_time = datetime.now()
     time_elapsed = end_time - start_time
-    print("Time Elapsed:", time_elapsed)
+    print(f"{end_time}: Time Elapsed:", time_elapsed)
     time_sc.iloc[3, 0] = time_elapsed
 
     # Identification of highly variable features (feature selection) ####
-    start_time = time.time()
+    start_time = datetime.now()
 
     sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5,
                                 n_top_genes = 1000)
@@ -102,10 +103,10 @@ def runTest(input: string):
     adata.raw = adata
     adata = adata[:, adata.var.highly_variable]
 
-    end_time = time.time()
+    end_time = datetime.now()
     time_elapsed = end_time - start_time
     adata
-    print("Normalization Time Elapsed:", time_elapsed)
+    print(f"{end_time}: Normalization Time Elapsed:", time_elapsed)
     time_sc.iloc[4, 0] = time_elapsed
 
     df = pd.DataFrame(adata.var.highly_variable, columns=['hvg'])
@@ -114,20 +115,20 @@ def runTest(input: string):
 
 
     # Scaling the data ####
-    start_time = time.time()
+    start_time = datetime.now()
 
     sc.pp.scale(adata, max_value=10)
 
-    end_time = time.time()
+    end_time = datetime.now()
     time_elapsed = end_time - start_time
-    print("Normalization Time Elapsed:", time_elapsed)
+    print(f"{end_time}: Normalization Time Elapsed:", time_elapsed)
     time_sc.iloc[5, 0] = time_elapsed
 
-    print( "PCA input size: ", adata.shape, flush=True)
+    print( f"{end_time}: PCA input size: ", adata.shape, flush=True)
     adata.write_csvs(inputPath.with_name( inputPath.stem + '_scaled'), False)
 
     # PCA ####
-    start_time = time.time()
+    start_time = datetime.now()
 
     sc.tl.pca(adata, svd_solver='arpack')
     #sc.pl.pca(adata, color='CST3')
@@ -135,40 +136,40 @@ def runTest(input: string):
     # adata.write(results_file) capire come definirlo
     adata
 
-    end_time = time.time()
+    end_time = datetime.now()
     time_elapsed = end_time - start_time
-    print("PCA Time Elapsed:", time_elapsed, flush=True)
+    print(f"{end_time}: PCA Time Elapsed:", time_elapsed, flush=True)
     time_sc.iloc[6, 0] = time_elapsed
     adata.write_csvs(inputPath.with_name( inputPath.stem + '_PCA'), False)
 
     # t-sne ####
-    start_time = time.time()
+    start_time = datetime.now()
 
     sc.tl.tsne(adata)
 
-    end_time = time.time()
+    end_time = datetime.now()
     time_elapsed = end_time - start_time
-    print("T-SNE Time Elapsed:", time_elapsed, flush=True)
+    print(f"{end_time}: T-SNE Time Elapsed:", time_elapsed, flush=True)
     time_sc.iloc[7, 0] = time_elapsed
 
     # UMAP ####
-    start_time = time.time()
+    start_time = datetime.now()
     sc.pp.neighbors(adata, n_neighbors=10, n_pcs=50)
     sc.tl.umap(adata)
 
-    end_time = time.time()
+    end_time = datetime.now()
     time_elapsed = end_time - start_time
-    print("UMAP Time Elapsed:", time_elapsed, flush=True)
+    print(f"{end_time}: UMAP Time Elapsed:", time_elapsed, flush=True)
     time_sc.iloc[8, 0] = time_elapsed
 
     # louvain ####
-    start_time = time.time()
+    start_time = datetime.now()
 
     sc.tl.louvain(adata, resolution = 0.13)
 
-    end_time = time.time()
+    end_time = datetime.now()
     time_elapsed = end_time - start_time
-    print("Louvain Time Elapsed:", time_elapsed, flush=True)
+    print(f"{end_time}: Louvain Time Elapsed:", time_elapsed, flush=True)
     time_sc.iloc[9, 0] = time_elapsed
 
     true_labels = adata.obs['Sample'].astype(str)
@@ -180,7 +181,7 @@ def runTest(input: string):
     ari_score = adjusted_rand_score(true_labels, predicted_labels)
 
     # Print the ARI score
-    print("Adjusted Rand Index (ARI):", ari_score)
+    print(f"{end_time}: Adjusted Rand Index (ARI):", ari_score)
 
     cluster_labels = adata.obs['louvain']
 
@@ -188,17 +189,17 @@ def runTest(input: string):
     silhouette_avg = silhouette_score(adata.X, cluster_labels)
 
     # Print the average silhouette score
-    print("Average Silhouette Score:", silhouette_avg)
+    print(f"{end_time}: Average Silhouette Score:", silhouette_avg)
 
 
     # leiden ####
-    start_time = time.time()
+    start_time = datetime.now()
 
     sc.tl.leiden(adata, resolution = 0.13)
 
-    end_time = time.time()
+    end_time = datetime.now() 
     time_elapsed = end_time - start_time
-    print("Leiden Time Elapsed:", time_elapsed, flush=True)
+    print(f"{end_time}: Leiden Time Elapsed:", time_elapsed, flush=True)
     time_sc.iloc[10, 0] = time_elapsed
 
     true_labels = adata.obs['Sample'].astype(str)
@@ -208,7 +209,7 @@ def runTest(input: string):
     ari_score = adjusted_rand_score(true_labels, predicted_labels)
 
     # Print the ARI score
-    print("Adjusted Rand Index (ARI):", ari_score)
+    print(f"{end_time}: Adjusted Rand Index (ARI):", ari_score)
 
     cluster_labels = adata.obs['leiden']
 
@@ -216,7 +217,7 @@ def runTest(input: string):
     silhouette_avg = silhouette_score(adata.X, cluster_labels)
 
     # Print the average silhouette score
-    print("Average Silhouette Score:", silhouette_avg)
+    print(f"{end_time}: Average Silhouette Score:", silhouette_avg)
 
     time_sc
 
